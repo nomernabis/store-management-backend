8b237fd79913603445c073842e7a8fd4e8f5ed38
pavel_cpp <pasha.dunin.94@gmail.com>
2019-03-25 11:38:08 +0600

    production changes

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%7@xu5*!wm5@57j@4sl==8f8)@_3!!nw!-)+8l9_^1tvojn1f7'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
